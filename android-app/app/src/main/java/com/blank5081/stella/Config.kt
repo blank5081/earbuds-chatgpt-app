ef9983a17e5cb431b01a8e0c8ec51c5edb0f8794
blank5081 <s8459120@gmail.com>
2025-11-15 11:57:53 +0530

    Add Config.kt

---
+++ android-app/app/src/main/java/com/blank5081/stella/Config.kt
@@ -0,0 +1,11 @@
+package com.blank5081.stella
+
+object Config {
+    // Replace this with your server URL later
+    // Example: "http://192.168.1.50:3333/converse"
+    const val SERVER_URL = "http://REPLACE_WITH_YOUR_SERVER/converse"
+
+    const val RECORD_FILENAME = "stella_record.3gp"
+    const val NOTIFICATION_CHANNEL_ID = "stella_channel"
+    const val NOTIFICATION_ID = 1001
+}
